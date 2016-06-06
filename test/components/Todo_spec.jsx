243afa6b111a6eefd7e1ba4e698dd403e896923c
Massimo Zambelli <mass.zambelli@gmail.com>
2016-06-06 02:49:50 +0200

    gitignore update + todoapp test

--- test/components/Todo_spec.jsx
+++ test/components/Todo_spec.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  renderIntoDocument
-} from 'react-addons-test-utils';
-//import Todo from '../../src/components/Todo';
+import {createRenderer} from 'react-addons-test-utils';
+import expect from 'expect';
+import Todo from '../../src/components/Todo';
+import MarkAllDone from '../../src/components/MarkAllDone';
+import TaskList from '../../src/components/TaskList';
+import AddTask from '../../src/components/AddTask';
+
+
+describe('Todo app', () => {
+  it('Todo renders the app DOM', () => {
+    let renderer = createRenderer();
+    renderer.render(<Todo />);
+    let actualElement = renderer.getRenderOutput();
+    let expectedElement = <div className="todo">
+        <MarkAllDone />
+        <hr />
+        <h1>Things I need to do...</h1>
+        <TaskList />
+        <AddTask />
+      </div>;
+    expect(actualElement).toEqual(expectedElement);
+  })
+})
